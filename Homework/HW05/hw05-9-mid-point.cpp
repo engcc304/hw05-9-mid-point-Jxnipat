cd7c9580e9b282908660e5af250099b4962e82d0
jxnipat <Jetnipat.prc@gmail.com>
2023-08-14 03:09:57 +0700

    upppppppp

--- Homework/HW05/hw05-9-mid-point.cpp
+++ Homework/HW05/hw05-9-mid-point.cpp
@@ -28,3 +28,32 @@
     Output:
         Mid Point of A and B is ( 4.5, -2.5 )
 */
+#include <stdio.h>
+
+int main() {
+    float x1, y1, x2, y2;
+
+    // รับพิกัดA
+    printf("Input A Point:\n");
+    printf("x1: ");
+    scanf("%f", &x1);
+    printf("y1: ");
+    scanf("%f", &y1);
+
+    // รับพิกัดB
+    printf("Input B Point:\n");
+    printf("x2: ");
+    scanf("%f", &x2);
+    printf("y2: ");
+    scanf("%f", &y2);
+
+    // คำนวณ
+    float midX = (x1 + x2) / 2;
+    float midY = (y1 + y2) / 2;
+
+    // แสดงผลลัพธ์
+    printf("Mid Point of A and B is ( %.1f, %.1f )\n", midX, midY);
+
+    return 0;
+    //เมือไหร่ HW เเม่งจะเสร็จหมดวะ
+}
